cabbc88c1bc9cb56fdb3f23752d5d0d0745ca0aa
peanutyumyum <jrimit@gmail.com>
2020-08-23 20:57:30 +0900

    ForeignKey lesson finished

--- eleventh_week_cd/doing/models.py
+++ eleventh_week_cd/doing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,3 +8,4 @@ class Jasoseol(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     update_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
